39af1dca22d5c4aee85851dd9dedbc0c55b23436
abdullah24555 <141515153+abdullah24555@users.noreply.github.com>
2023-08-05 22:22:58 +0500

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['94740804536', 'Dinuwa Official²⁰²³', true],
-  ['94740804536', 'Dinuwa Official²⁰²³', true], 
+  ['03069051683', 'abdullah, true],
+  ['03069051683', 'abdullah, true], 
   [''] 
 ] //Number of owners
 
